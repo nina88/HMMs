125b3732fe8ff1f70a61b35341328da6cd83be06
nina88 <nina88@ncl>
2012-07-25 14:27:09 +0100

    adding tests for read_FASTA and combine_FASTA

--- inst/tests/test-4.R
+++ inst/tests/test-4.R
@@ -5,3 +5,16 @@ test_that("test convert", {
   expect_that(convert(LETTERS[c(1,3:9,11:14, 16:20,22,23,25)],5),  throws_error())
   })
 
+test_that("test read_FASTA", {
+  x=read_FASTA("inst/extdata/p53.txt",2)
+  expect_that(class(x), matches("hmm_fasta"))
+})
+
+test_that("test combine_FASTA", {
+  x=read_FASTA("inst/extdata/p53.txt",2)
+  y=combine_FASTA(x,x)
+  expect_that(class(y), matches("hmm_fasta"))
+  expect_that(length(y$join), equals(2*length(x$join)-1))
+  expect_that(length(y$fasta_seq), equals(2*length(x$fasta_seq)))
+})
+
