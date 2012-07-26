daf9f8aab129706fb6fc3284be7d975348a0f254
nina88 <nina88@ncl>
2012-07-26 12:26:13 +0100

    more changes to checkpointing

--- R/gibbs.R
+++ R/gibbs.R
@@ -1,172 +1,176 @@
 #initialise_output
-#initialise_checkpoint = function(hour) {
-    #do something
-  #  class("hmm_checkpoint")
-  #  return(somethiong)
-#}
 
-#ic  = initialise_checkpoint(10)
-#gibbs(y, iter, ic)
-gibbs <- function(y, iter, hour, prior, r)##, checkpoint =NULL, prior)
+gibbs <- function(y, iter, prior, r, checkpoint = NULL)
 {
-    if (class(y)!="hmm_fasta"){
-        stop("Object y not from correct class")
+  if (class(y)!="hmm_fasta"){
+    stop("Object y not from correct class")
     }
-    
-    if (class(prior)!="prior_parameters"){
-      stop("Object prior not from correct class")
+  
+  if (class(prior)!="prior_parameters"){
+    stop("Object prior not from correct class")
     }
-    
-    if(!is.null(checkpoint)){
-        
+  
+  if (is.null(checkpoint)){
+    message("Note that you are not checkpointing, removing a burn in, or thinning")
+    checkpoint=initialise_checkpoint(1,1,1)
     }
-    f=y$level
-    if (length(y$join)==2) {
-        joins=0
+  
+  if(class(checkpoint)!="hmm_checkpoint"){
+    stop("Object checkpoint not from correct class")    
+    }
+  
+  if (length(y$join)==2) {
+    joins=0
     } else { 
-        joins=y$join[2:(length(y$join)-1)]
-    } 
-    y=y$fasta_seq
-    y=factor(y,levels=1:f)
+      joins=y$join[2:(length(y$join)-1)]
+      } 
+  
+  #### find sequence and f and length of y (n)
+  f=y$level
+  y=y$fasta_seq
+  y=factor(y,levels=1:f)
+  n = length(y)
+  
+  ##### check for lambda and P existing/ initialise them
+  init = initialise(prior, f, r)
+  lambda = init$lambda
+  P = init$P
+  count=init$count
+  segment1=init$segment1
+  
+  ### Prior parameter for lambda
+  b=prior$b
+  a=prior$a
     
-    ##### check for lambda and P existing/ initialise them
-    init = initialise(prior, f, r)
-    lambda = init$lambda
-    P = init$P
-    count=init$count
-    segment1=init$segment1
-    ### Prior parameter for lambda
-    b=prior$b
-    a=prior$a
-    #### check not finished
-    if (count==iter+1) break
-    n = length(y)
-    posterior.temp=numeric(hour)
-    lambda.store=matrix(0,nrow=hour, ncol=r^2)
-    P.store=matrix(0,nrow=hour, ncol=r*f^2)
-    segment.store=matrix(0, nrow=hour, ncol=n)
-    if ((count-1)==1){
-        lambda.store[1,]=as.vector(lambda)
-        P.store[1,]=as.vector(P)
+  #### check not finished
+  if (count==iter+1) break
+  
+  #### create storage vectors and matrices
+  posterior.temp=numeric(hour)
+  lambda.store=matrix(0,nrow=hour, ncol=r^2)
+  P.store=matrix(0,nrow=hour, ncol=r*f^2)
+  segment.store=matrix(0, nrow=hour, ncol=n)
+  
+  #### store initial values
+  if ((count-1)==1){
+    lambda.store[1,]=as.vector(lambda)
+    P.store[1,]=as.vector(P)
     }
+  
+  ##### FB and dirichlets
+  for (i in count:iter) {
+    print(i)
     
-    ##### FB and dirichlets
-    for (i in count:iter) {
+    ##### Forward backward
+    st = FB(y, lambda, P)
+    segment2 = st$s
         
-        print(i)
-        ##### Forward backward
-        st = FB(y, lambda, P)
-        segment2 = st$s
+    #### label switching check
+    if (i==2){
+      segment2 = segment2
+      P = P
+      lambda = lambda
+      }
+    else {
+      segm = label_switch(segment2, segment1, r,lambda, P, f)
+      segment2 = segm$segment.store2
+      P = segm$P
+      lambda = segm$lambda
+      }
+    segment2 = factor(segment2, levels = 1:r)
+    y = factor(y, levels = 1:f)
+    segment1 = segment2
         
-        #### label switching check
-        if (i==2){
-            segment2 = segment2
-            P = P
-            lambda = lambda
+    ### find parameters for dirichlets
+    s.trans = table(segment2[1:(n-1)], segment2[2:n]) 
+    y.trans = table(y[1:(n-1)], y[2:n], segment2[2:n]) 
+        
+    # take off transitions between proteins if real data
+    if(all(joins!=0)){
+      for (m in 1:length(joins)){
+        s.trans[segment2[joins[m]], segment2[joins[m]+1]] = s.trans[segment2[joins[m]], segment2[joins[m]+1]]-1
+        y.trans[y[joins[m]], y[joins[m]+1],segment2[joins[m]+1]] = y.trans[y[joins[m]], y[joins[m]+1],segment2[joins[m]+1]]-1
         }
-        else {
-            segm = label_switch(segment2, segment1, r,lambda, P, f)
-            segment2 = segm$segment.store2
-            P = segm$P
-            lambda = segm$lambda
+      }
+    #### simulate from dirichlets
+    ### find p
+    for (k in 1:r){
+      for (j in 1:f){
+        P[j,,k] = rdiric(1, a+y.trans[j,,k])
         }
+      }
+    ### find lambda
+    for (k in 1:r){
+      lambda[k,] = rdiric(1, b[k,]+s.trans[k,])
+      }
+    ### log prior
+    P.prior = sum((a-1)*log(P))+r*f*lgamma(f*a)-r*(f^2)*lgamma(a)
+    lambda.prior = sum((b-1)*log(lambda))+r*lgamma(sum(b[1,]))-r*sum(lgamma(b[1,]))
+    log.prior = P.prior+lambda.prior
         
-        segment2 = factor(segment2, levels = 1:r)
-        y = factor(y, levels = 1:f)
-        segment1 = segment2
-        
-        ### find parameters for dirichlets
-        s.trans = table(segment2[1:(n-1)], segment2[2:n]) 
-        y.trans = table(y[1:(n-1)], y[2:n], segment2[2:n]) 
-        
-        # take off transitions between proteins if real data
-        if(all(joins!=0)){
-            for (m in 1:length(joins)){
-                s.trans[segment2[joins[m]], segment2[joins[m]+1]] = s.trans[segment2[joins[m]], segment2[joins[m]+1]]-1
-                y.trans[y[joins[m]], y[joins[m]+1],segment2[joins[m]+1]] = y.trans[y[joins[m]], y[joins[m]+1],segment2[joins[m]+1]]-1
-            }
+    ###### calculate log likelihood for transition probabilities (P)
+    P.loglike = numeric(r)
+    for (l in 1:r){
+      P.loglike[l]=sum(y.trans[,,l]*log(P[,,l]))
+      }
+    ###### calculate log likelihood for transition probabilities (lambda)
+    lambda.loglike = sum(s.trans*log(lambda))
+    
+    ##### find log like and log post
+    loglike.store = sum(P.loglike)+lambda.loglike+log(1/(f*r))
+    post = loglike.store+log.prior
+    
+    if (i%%hour==0){
+      posterior.temp[hour]=post
+      P.store[hour,]=as.vector(P)
+      lambda.store[hour,]=as.vector(lambda)
+      segment.store[hour,]=segment2
+      } else {
+        posterior.temp[i%%hour]=post
+        P.store[i%%hour,]=as.vector(P)
+        lambda.store[i%%hour,]=as.vector(lambda)
+        segment.store[i%%hour,]=segment2
         }
-        
-        #### simulate from dirichlets
-        ### find p
-        for (k in 1:r){
-            for (j in 1:f){
-                P[j,,k] = rdiric(1, a+y.trans[j,,k])
-            }
-        }
-        ### find lambda
-        for (k in 1:r){
-            lambda[k,] = rdiric(1, b[k,]+s.trans[k,])
-        }
-        
-        ### log prior
-        P.prior = sum((a-1)*log(P))+r*f*lgamma(f*a)-r*(f^2)*lgamma(a)
-        lambda.prior = sum((b-1)*log(lambda))+r*lgamma(sum(b[1,]))-r*sum(lgamma(b[1,]))
-        log.prior = P.prior+lambda.prior
-        
-        ###### calculate log likelihood for transition probabilities (P)
-        P.loglike = numeric(r)
-        for (l in 1:r){
-            P.loglike[l]=sum(y.trans[,,l]*log(P[,,l]))
-        }
-        
-        ###### calculate log likelihood for transition probabilities (lambda)
-        lambda.loglike = sum(s.trans*log(lambda))
-        
-        ##### find log like and log post
-        loglike.store = sum(P.loglike)+lambda.loglike+log(1/(f*r))
-        post = loglike.store+log.prior
-        if (i%%hour==0){
-            posterior.temp[hour]=post
-            P.store[hour,]=as.vector(P)
-            lambda.store[hour,]=as.vector(lambda)
-            segment.store[hour,]=segment2
-        } else{
-            posterior.temp[i%%hour]=post
-            P.store[i%%hour,]=as.vector(P)
-            lambda.store[i%%hour,]=as.vector(lambda)
-            segment.store[i%%hour,]=segment2
-        }
-        
-        ## write lambda and P to file for checkpointing
-        if (i%%hour==0){
-            checkpoint(lambda, P,segment1, posterior.temp, P.store, lambda.store, segment.store, i, r)
-        }
+    ## write lambda and P to file for checkpointing
+    if (i%%hour==0){
+      checkpoint(lambda, P,segment1, posterior.temp, P.store, lambda.store, segment.store, i, r)
+      }
     }  
-    return(list(lambda = lambda, P = P))
+  return(list(lambda = lambda, P = P))
 }
 
 ##Change .txt to csv
 ##Pass output files as arguments
 ##Restart argument =TRUE or FALSE
-##Pass prior arguments in.
 ##Change to initialise_state
-##Have initialise_checkpoint
+
 
 initialise<- function(prior, f, r)
 {
-    if (file.exists("checkpoint.Rdata")==T){
-            message("Using existing files")
-            load("checkpoint.Rdata")
+  if (file.exists("checkpoint.Rdata")==T){
+    message("Using existing files")
+    load("checkpoint.Rdata")
     } else {
-        message("Making files")
-        transition_matrices = initialise_transition_matrices(prior, r, f)
-        lambda = transition_matrices$lambda
-        P = transition_matrices$P
-        count = 2
-        segment1 = NA
-    }
-    return(list(lambda = lambda, P = P, count = count, segment1 = segment1))
+      message("Making files")
+      transition_matrices = initialise_transition_matrices(prior, r, f)
+      lambda = transition_matrices$lambda
+      P = transition_matrices$P
+      count = 2
+      segment1 = NA
+      }
+  return(list(lambda = lambda, P = P, count = count, segment1 = segment1))
 }
 
 #initialise_transition_matrices
 initialise_transition_matrices <- function(prior, r, f)
-{ # P
+{ 
+  # P
   a=prior$a
   P=array(0,c(f,f,r))
   for(j in 1:r){
     P[,,j] = rdiric(f,rep(a,f))
   }
+  
   ### lambda 
   b = prior$b
   diag(b) = c
@@ -189,18 +193,27 @@ initialise_prior <- function(a, mu, s, r)
   return(prior)
 }
 
-
-
-##Create Checkpoint.RData file using load/save
-##Include everything you need to checkpoint
-##Store thin, burnin
+##Store thin, burnin in checkpointing
 
 checkpoint <- function(lambda, P, segment1, posterior.temp, P.store, lambda.store, segment.store, i, r)
 {
-    count=i+1
-    save(lambda,P,segment1,count,file="checkpoint.Rdata")
-    write.table(posterior.temp,file=paste("output",r,sep=""),append=T,row.names=F,col.names=F)
-    write.table(P.store,file=paste("P.store",r,sep=""),append=T,row.names=F,col.names=F)
-    write.table(lambda.store,file=paste("lambda.store",r,sep=""),append=T,row.names=F,col.names=F)
-    write.table(segment.store,file=paste("segment.store",r,sep=""),append=T,row.names=F,col.names=F)	
+  count=i+1
+  save(lambda,P,segment1,count,file="checkpoint.Rdata")
+  write.table(posterior.temp,file=paste("output",r,sep=""),append=T,row.names=F,col.names=F)
+  write.table(P.store,file=paste("P.store",r,sep=""),append=T,row.names=F,col.names=F)
+  write.table(lambda.store,file=paste("lambda.store",r,sep=""),append=T,row.names=F,col.names=F)
+  write.table(segment.store,file=paste("segment.store",r,sep=""),append=T,row.names=F,col.names=F)	
+}
+
+
+#########
+initialise_checkpoint = function(burnin, thin, hour) 
+{
+  if (hour%%thin!=0){
+    stop("Hour must be a multiple of thin")
+  }
+  
+  checkpoint=list(burnin,thin, hour)
+  class("hmm_checkpoint")
+  return(checkpoint)
 }
