6a5fe1a4c50e6054002aa3ce74628a2cd0a680cc
Colin Gillespie <csgillespie@gmail.com>
2012-11-13 15:41:50 +0000

    Adding in p53 data

---
+++ R/p53.R
@@ -0,0 +1,8 @@
+#' @name p53
+#' @title p53 FASTA data
+#' @description The frequency of occurrence of unique words in the novel Moby Dick by Herman Melville.
+#' @docType data
+#' @usage moby
+#' @format A vector
+#' @source M. E. J. Newman, "Power laws, Pareto distributions and Zipf's law." Contemporary Physics 46, 323 (2005). See http://tuvalu.santafe.edu/~aaronc/powerlaws/data.htm for further details.
+NULL
