8ff4e306c75e1017bd88c4445a8dd94bc3657a94
Colin Gillespie <csgilespie@gmail.com>
2012-11-15 21:42:39 +0000

    Combining help files.

--- R/p53.R
+++ R/p53.R
@@ -1,72 +1,10 @@
 #' @name p53
-#' @title p53 FASTA data
-#' @description The amino acid sequence of p53
+#' @aliases mdm2 CBP TAF15 FUS EWS Bnip3 NBR1 SQSTM1
+#' @title Example FASTA sequences
+#' @description The amino acid sequence of p53, mdm2, CBP, TAF15, FUS, EWS, 
+#' Bnip3, NBR1, SQSTM1. To load a particular sequence, use the data function. 
+#' For example, data(p53)
 #' @docType data
-#' @usage p53
 #' @format Vector of strings
-#' @source online database 
-NULL
-#' @name mdm2
-#' @title mdm2 FASTA data
-#' @description The amino acid sequence of mdm2
-#' @docType data
-#' @usage mdm2
-#' @format Vector of strings
-#' @source online database 
-NULL
-#' @name CBP
-#' @title CBP FASTA data
-#' @description The amino acid sequence of CBP
-#' @docType data
-#' @usage CBP
-#' @format Vector of strings
-#' @source online database 
-NULL
-#' @name TAF15
-#' @title TAF15 FASTA data
-#' @description The amino acid sequence of TAF15
-#' @docType data
-#' @usage TAF15
-#' @format Vector of strings
-#' @source online database 
-NULL
-#' @name FUS
-#' @title FUS FASTA data
-#' @description The amino acid sequence of FUS
-#' @docType data
-#' @usage FUS
-#' @format Vector of strings
-#' @source online database 
-NULL
-#' @name EWS
-#' @title EWS FASTA data
-#' @description The amino acid sequence of EWS
-#' @docType data
-#' @usage EWS
-#' @format Vector of strings
-#' @source online database 
-NULL
-#' @name Bnip3
-#' @title Bnip3 FASTA data
-#' @description The amino acid sequence of Bnip3
-#' @docType data
-#' @usage Bnip3
-#' @format Vector of strings
-#' @source online database 
-NULL
-#' @name NBR1
-#' @title NBR1 FASTA data
-#' @description The amino acid sequence of EWS
-#' @docType data
-#' @usage NBR1
-#' @format Vector of strings
-#' @source online database 
-NULL
-#' @name SQSTM1
-#' @title SQSTM1 FASTA data
-#' @description The amino acid sequence of SQSTM1
-#' @docType data
-#' @usage SQSTM1
-#' @format Vector of strings
-#' @source online database 
+#' @source online database  NEED MORE DETAILS!!!
 NULL
