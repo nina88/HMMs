fc4b76806881326d156431c77d0c1903dbcb7ec2
Colin Gillespie <csgilespie@gmail.com>
2012-11-15 21:21:00 +0000

    Deleting old files

--- inst/tests/test-1.R
+++
@@ -1,9 +0,0 @@
-test_that("trigonometric functions match identies", {
-expect_that(sin(pi / 4), equals(1 / sqrt(2)))
-expect_that(cos(pi / 4), equals(1 / sqrt(2)))
-expect_that(tan(pi / 4), equals(1))
-})
-
-test_that("trigonometric functions match identities", {
-expect_that(sin(pi / 4), equals(1))
-})
