03a2c4d669920bac020495383b8c484c0c2e3c46
nina88 <nina88@ncl>
2012-11-15 10:31:28 +0000

    data files

--- R/p53.R
+++ R/p53.R
@@ -1,8 +1,72 @@
 #' @name p53
 #' @title p53 FASTA data
-#' @description The frequency of occurrence of unique words in the novel Moby Dick by Herman Melville.
+#' @description The amino acid sequence of p53
 #' @docType data
-#' @usage moby
-#' @format A vector
-#' @source M. E. J. Newman, "Power laws, Pareto distributions and Zipf's law." Contemporary Physics 46, 323 (2005). See http://tuvalu.santafe.edu/~aaronc/powerlaws/data.htm for further details.
+#' @usage p53
+#' @format Vector of strings
+#' @source online database 
+NULL
+#' @name mdm2
+#' @title mdm2 FASTA data
+#' @description The amino acid sequence of mdm2
+#' @docType data
+#' @usage mdm2
+#' @format Vector of strings
+#' @source online database 
+NULL
+#' @name CBP
+#' @title CBP FASTA data
+#' @description The amino acid sequence of CBP
+#' @docType data
+#' @usage CBP
+#' @format Vector of strings
+#' @source online database 
+NULL
+#' @name TAF15
+#' @title TAF15 FASTA data
+#' @description The amino acid sequence of TAF15
+#' @docType data
+#' @usage TAF15
+#' @format Vector of strings
+#' @source online database 
+NULL
+#' @name FUS
+#' @title FUS FASTA data
+#' @description The amino acid sequence of FUS
+#' @docType data
+#' @usage FUS
+#' @format Vector of strings
+#' @source online database 
+NULL
+#' @name EWS
+#' @title EWS FASTA data
+#' @description The amino acid sequence of EWS
+#' @docType data
+#' @usage EWS
+#' @format Vector of strings
+#' @source online database 
+NULL
+#' @name Bnip3
+#' @title Bnip3 FASTA data
+#' @description The amino acid sequence of Bnip3
+#' @docType data
+#' @usage Bnip3
+#' @format Vector of strings
+#' @source online database 
+NULL
+#' @name NBR1
+#' @title NBR1 FASTA data
+#' @description The amino acid sequence of EWS
+#' @docType data
+#' @usage NBR1
+#' @format Vector of strings
+#' @source online database 
+NULL
+#' @name SQSTM1
+#' @title SQSTM1 FASTA data
+#' @description The amino acid sequence of SQSTM1
+#' @docType data
+#' @usage SQSTM1
+#' @format Vector of strings
+#' @source online database 
 NULL
