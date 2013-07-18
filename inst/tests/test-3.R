a228a7dbbf00167b301268547a47d1462c7d17ba
nina88 <nina88@ncl>
2013-07-18 14:39:58 +0100

    update

--- inst/tests/test-3.R
+++ inst/tests/test-3.R
@@ -1,74 +1,25 @@
-test_that("check equilbrium distribution", {
-  lambda <- matrix(c(0.999,0.001,0.001,0.999),ncol=2,byrow=TRUE)
-  eqdist <- equil(lambda)
-  expect_that(eqdist, equals(c(0.5,0.5)))
-})
-
-test_that("label-switching", {
-  ### when label switching should happen
-  lambda <- matrix(c(1,2,3,4),ncol=2,byrow=TRUE)
-  P.1 <- matrix(c(1,2,3,4),ncol=2,byrow=TRUE)
-  P.2 <- matrix(c(5,6,7,8),ncol=2,byrow=TRUE)
+test_that("test FB", {
+  lambda <- matrix(c(0.3,0.7,0.6,0.4),ncol=2,byrow=TRUE)
+  P.1 <- matrix(c(0.3,0.7,0.2,0.8),ncol=2,byrow=TRUE)
+  P.2 <- matrix(c(0.5,0.5,0.4,0.6),ncol=2,byrow=TRUE)
   P <- array(0,c(2,2,2))
   P[,,1] <- P.1
   P[,,2] <- P.2
-  x=label_switch(c(1,2,1,2),c(2,1,2,1), 2, lambda, P, 2)
-  expect_that(as.vector(x$lambda), equals(c(4,2,3,1)))
-  expect_that(x$segment.store2, equals(c(2,1,2,1)))
-  expect_that(as.vector(x$P),equals(c(5,7,6,8,1,3,2,4)))
-  ### when label switching shouldnt happen
-  x=label_switch(c(1,2,1,2),c(1,2,1,2), 2, lambda, P, 2)
-  expect_that(x$lambda, equals(lambda))
-  expect_that(x$segment.store2, equals(c(1,2,1,2)))
-  expect_that(x$P,equals(P))
-  #### check happens when having to check max matches - label switching should happen
-  x=label_switch(c(1,1,1,1,2,2,2),c(2,2,2,2,1,1,1), 2, lambda, P, 2)
-  expect_that(as.vector(x$lambda), equals(c(4,2,3,1)))
-  expect_that(x$segment.store2, equals(c(2,2,2,2,1,1,1)))
-  expect_that(as.vector(x$P),equals(c(5,7,6,8,1,3,2,4)))
-  ### label switching shouldnt happen
-  x=label_switch(c(1,1,1,1,2,2,2),c(1,1,1,2,2,2,2), 2, lambda, P, 2)
-  expect_that(x$lambda, equals(lambda))
-  expect_that(x$segment.store2, equals(c(1,1,1,1,2,2,2)))
-  expect_that(x$P,equals(P))
-  
-})
+  x=FB(c(1,1,2), lambda, P)
+  expect_that(round(x$back,7),  equals(array(c(0.1764706, 0.8235294, 0.4285714, 0.5714286, 0.2379471,  0.7620529, 0.5221843, 0.4778157,0,0,0,0),c(2,2,3))))
+  expect_that(round(x$f,7),  equals(matrix(c(0.3, 0.3844221, 0.5683559, 0.7, 0.6155779, 0.4316441),byrow=TRUE,nrow=2)))
+  })
 
-test_that("check dirichlet random number generator", {
-  x=rdiric(1000000,c(1,1))
-  expect_that(round(mean(x[,1]),1), equals(round(mean(x[,2]),1)))
-})
-
-test_that("permut function returns correct sequences", {
-  y=c(1,1,2,3,3)
-  x=permut(3,y,1)
-  expect_that(x, equals(y))
-  x=permut(3,y,2)
-  expect_that(x, equals(c(1,1,3,2,2)))
-  x=permut(3,y,3)
-  expect_that(x, equals(c(2,2,1,3,3)))
-  x=permut(3,y,4)
-  expect_that(x, equals(c(2,2,3,1,1)))
-  x=permut(3,y,5)
-  expect_that(x, equals(c(3,3,1,2,2)))
-  x=permut(3,y,6)
-  expect_that(x, equals(c(3,3,2,1,1)))
-})
-
-
-test_that("check permutation function", {
-  y=matrix(c(1,2,2,1),ncol=2,nrow=2,byrow=TRUE)
-  x=permutations(2,2)
-  expect_that(x, equals(y))
-  y=matrix(c(1,2,3,1,3,2,2,1,3,2,3,1,3,1,2,3,2,1),ncol=3,nrow=6,byrow=TRUE)
-  x=permutations(3,3)
-  expect_that(x, equals(y))
-})
-
-test_that("check order_of_firsts", {
-  expect_that(order_of_firsts(c(2,2,2,2,3,3),3), equals(c(2,3,1)))
-  expect_that(order_of_firsts(c(3,3,2,2,3,3,1),3), equals(c(3,2,1)))
-  expect_that(order_of_firsts(c(1,1,1,3,2,3,3,2,2,4),4), equals(c(1,3,2,4)))
+test_that("test powerFB", {
+  lambda <- matrix(c(0.3,0.7,0.6,0.4),ncol=2,byrow=TRUE)
+  P.1 <- matrix(c(0.3,0.7,0.2,0.8),ncol=2,byrow=TRUE)
+  P.2 <- matrix(c(0.5,0.5,0.4,0.6),ncol=2,byrow=TRUE)
+  P <- array(0,c(2,2,2))
+  P[,,1] <- P.1
+  P[,,2] <- P.2
+  x=FBpower(c(1,1,2), lambda, P, 0.5)
+  expect_that(round(x$back,7),  equals(array(c(0.2325672,0.7674328,  0.5147186,  0.4852814, 0.2686629,0.7313371, 0.5625078,  0.4374922, 0,0,0,0),c(2,2,3))))
+  expect_that(round(x$f,7),  equals(matrix(c(0.3773705, 0.4235371, 0.5149669, 0.6226295, 0.5764629, 0.4850331),byrow=TRUE,nrow=2)))
 })
 
 
